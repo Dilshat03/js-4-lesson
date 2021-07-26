89f322886f41aeda7ea84f6672131acb683f9b49
Aiupov Dilshat <kemelov.dilsha@gmail.com>
2021-07-26 13:15:52 +0300

    initial

---
+++ script.js
@@ -0,0 +1,80 @@
+// let str = "Lorem ipsum"
+// if ( str.includes("a")) {
+//     console.log("yes")
+// } else {
+//     console.log("no")
+// }
+//
+// let str1 = "It Dubai"
+// if ( str1.includes("a")) {
+//     console.log("yes")
+// } else {
+//     console.log("no")
+// }
+//
+// let str3 = "Moscow"
+// if ( str3.includes("a")) {
+//     console.log("yes")
+// } else {
+//     console.log("no")
+// }
+
+
+//Functions
+// function sum(a, b) {
+//     return a + b
+// }
+//
+// console.log(sum(10,20,30,40))
+// console.log(sum(10,90))
+// console.log(sum(10,55))
+//
+// function square (num) {
+//     return num * num
+// }
+//
+// console.log(square(5))
+// console.log(square(9))
+// console.log(square(3))
+
+
+// function checkSymbol(str) {
+//     if (str.includes("a")) {
+//         return "yes"
+//     } else {
+//         return "no"
+//     }
+// }
+//
+// console.log(checkSymbol("Moscow"))
+// console.log(checkSymbol("Kant"))
+// console.log(checkSymbol("Dubai"))
+// console.log(checkSymbol("Manchester"))
+
+
+// function checkNumber (num){
+//     if (num > 0) {
+//         return "positive"
+//     } else if ( num < 0) {
+//         return "negative"
+//     }
+//     return "0"
+// }
+//
+// console.log(checkNumber(0))
+// console.log(checkNumber(2))
+// console.log(checkNumber(-2))
+
+
+// function greetingName(name){
+//    return `Hello my dear ${name}`
+// }
+//
+//
+// console.log(greetingName("Иван"))
+
+
+//Фактическое значение которое передается в функцию называется аргумент
+//Имя по которому вы принимаете фактическое значение это параметры
+
+
